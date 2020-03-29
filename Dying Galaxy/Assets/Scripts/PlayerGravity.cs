7f7569f5ccafc3e7b1e5f472bdcee28910385d67
MatheusMAssis <assis.matheus1@gmail.com>
2020-03-29 15:40:49 -0300

    starting project

---
+++ Dying Galaxy/Assets/Scripts/PlayerGravity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGravity : MonoBehaviour
+{
+    public PlanetScript attractorPlanet;
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (attractorPlanet)
+        {
+            attractorPlanet.Attract(playerTransform);
+        }
+    }
+}
